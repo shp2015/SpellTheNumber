13acf90a06e1b9c7946e027f9892078da7c8011c
Shashank <shashankp@unifytech.com>
2020-05-30 18:20:55 +0530

    Removed Switch case statements and replaced it with Dictionary objects Dictionary objects initialized with Constructor

--- Convert.cs
+++ Convert.cs
@@ -1,9 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SpellTheNumber
 {
     class Convert
     {
+        private Dictionary<long, string> uniqueNameDictionary = new Dictionary<long, string>();
+        private Dictionary<int, int> NumberToDivAndModDictionary = new Dictionary<int, int>();
+        public Convert()
+        {
+            InitializeDictionaryObjects();
+        }
+        //Initialize the dictionary objects at the time of initializing the class
+        private void InitializeDictionaryObjects()
+        {
+            InitializeUniqueNameDictionary();
+            InitializeNumberToDivAndModDictionary();
+        }
+        private void InitializeUniqueNameDictionary()
+        {
+            uniqueNameDictionary.Add(0, "Zero");
+            uniqueNameDictionary.Add(1, "One");
+            uniqueNameDictionary.Add(2, "Two");
+            uniqueNameDictionary.Add(3, "Three");
+            uniqueNameDictionary.Add(4, "Four");
+            uniqueNameDictionary.Add(5, "Five");
+            uniqueNameDictionary.Add(6, "Six");
+            uniqueNameDictionary.Add(7, "Seven");
+            uniqueNameDictionary.Add(8, "Eight");
+            uniqueNameDictionary.Add(9, "Nine");
+            uniqueNameDictionary.Add(10, "Ten");
+            uniqueNameDictionary.Add(11, "Eleven");
+            uniqueNameDictionary.Add(12, "Twelve");
+            uniqueNameDictionary.Add(13, "Thirteen");
+            uniqueNameDictionary.Add(14, "Fourteen");
+            uniqueNameDictionary.Add(15, "Fifteen");
+            uniqueNameDictionary.Add(16, "Sixteen");
+            uniqueNameDictionary.Add(17, "Seventeen");
+            uniqueNameDictionary.Add(18, "Eighteen");
+            uniqueNameDictionary.Add(19, "Nineteen");
+            uniqueNameDictionary.Add(20, "Twenty");
+            uniqueNameDictionary.Add(30, "Thirty");
+            uniqueNameDictionary.Add(40, "Forty");
+            uniqueNameDictionary.Add(50, "Fifty");
+            uniqueNameDictionary.Add(60, "Sixty");
+            uniqueNameDictionary.Add(70, "Seventy");
+            uniqueNameDictionary.Add(80, "Eighty");
+            uniqueNameDictionary.Add(90, "Ninty");
+            uniqueNameDictionary.Add(100, "Hundrad");
+            uniqueNameDictionary.Add(1000, "Thousand");
+            uniqueNameDictionary.Add(100000, "Lakh");
+            uniqueNameDictionary.Add(10000000, "Crore");
+            uniqueNameDictionary.Add(1000000000, "Arab");
+        }
+        private void InitializeNumberToDivAndModDictionary()
+        {
+            NumberToDivAndModDictionary.Add(2, 10);
+            NumberToDivAndModDictionary.Add(3, 100);
+            NumberToDivAndModDictionary.Add(4, 1000);
+            NumberToDivAndModDictionary.Add(5, 1000);
+            NumberToDivAndModDictionary.Add(6, 100000);
+            NumberToDivAndModDictionary.Add(7, 100000);
+            NumberToDivAndModDictionary.Add(8, 10000000);
+            NumberToDivAndModDictionary.Add(9, 10000000);
+            NumberToDivAndModDictionary.Add(10, 1000000000);
+            NumberToDivAndModDictionary.Add(11, 1000000000);
+            NumberToDivAndModDictionary.Add(12, 10000000);
+            NumberToDivAndModDictionary.Add(13, 10000000);
+            NumberToDivAndModDictionary.Add(14, 10000000);
+            NumberToDivAndModDictionary.Add(15, 10000000);
+            NumberToDivAndModDictionary.Add(16, 10000000);
+            NumberToDivAndModDictionary.Add(17, 10000000);
+        }
+        /// <summary>
+        /// Function to pass word format for numbers
+        /// </summary>
+        /// <param name="numericFormat"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
         public string ConvertNumericToWordFormat(long numericFormat, int length)
         {
             string wordFormat = "";
@@ -47,76 +121,7 @@ namespace SpellTheNumber
         /// <returns></returns>
         private string GetUniqueNamesForNumbers(long number)
         {
-            switch (number)
-            {
-                case 0:
-                    return "Zero";
-                case 1:
-                    return "One";
-                case 2:
-                    return "Two";
-                case 3:
-                    return "Three";
-                case 4:
-                    return "Four";
-                case 5:
-                    return "Five";
-                case 6:
-                    return "Six";
-                case 7:
-                    return "Seven";
-                case 8:
-                    return "Eight";
-                case 9:
-                    return "Nine";
-                case 10:
-                    return "Ten";
-                case 11:
-                    return "Eleven";
-                case 12:
-                    return "Twelve";
-                case 13:
-                    return "Thirteen";
-                case 14:
-                    return "Fourteen";
-                case 15:
-                    return "Fifteen";
-                case 16:
-                    return "Sixteen";
-                case 17:
-                    return "Seventeen";
-                case 18:
-                    return "Eighteen";
-                case 19:
-                    return "Nineteen";
-                case 20:
-                    return "Twenty";
-                case 30:
-                    return "Thirty";
-                case 40:
-                    return "Forty";
-                case 50:
-                    return "Fifty";
-                case 60:
-                    return "Sixty";
-                case 70:
-                    return "Seventy";
-                case 80:
-                    return "Eighty";
-                case 90:
-                    return "Ninty";
-                case 100:
-                    return "Hundrad";
-                case 1000:
-                    return "Thousand";
-                case 100000:
-                    return "Lakh";
-                case 10000000:
-                    return "Crore";
-                case 1000000000:
-                    return "Arab";
-            }
-            return "";
+            return uniqueNameDictionary[number];
         }
         /// <summary>
         /// This functions returns the number with which division number and remainder number needs to be calculated
@@ -125,28 +130,7 @@ namespace SpellTheNumber
         /// <returns></returns>
         private long GetValueForDivAndMod(int length)
         {
-            switch (length)
-            {
-                case 2:
-                    return 10;
-                case 3:
-                    return 100;
-                case 4:
-                case 5:
-                    return 1000;
-                case 6:
-                case 7:
-                    return 100000;
-                case 8:
-                case 9:
-                    return 10000000;
-                case 10:
-                case 11:
-                    return 1000000000;
-                default:
-                    return 10000000;
-            }
-            return 0;
+            return NumberToDivAndModDictionary[length];
         }
     }
 }
